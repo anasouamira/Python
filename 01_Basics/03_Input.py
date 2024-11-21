c73622e9cdaba8184934c1b9d31b443e5274f17e
Ouamira <anasouamira@gmail.com>
2024-11-21 17:15:25 +0100

    Created By Anas Ouamira

---
+++ 01_Basics/03_Input.py
@@ -0,0 +1,9 @@
+# input() = A function that prompts the user to enter data
+#           Returns the entered data as a string 
+
+name = input("what your name : " )
+age = int(input("Howe old are you? : "))
+
+print(f" Name : {name}")
+print(f"Age : {age}")
+
