65557959b52e93ddd37d09cd0901fbfa3d5aaa9e
Ouamira <anasouamira@gmail.com>
2024-11-21 17:01:10 +0100

    Created By Anas Ouamira

---
+++ 01_Basics/02_TypeCasting.py
@@ -0,0 +1,15 @@
+# TypeCasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "Anas ouamira"
+age = "20"
+gpa = "3.2"
+is_student = True
+
+# converting a variables:
+age = int(age)
+gpa = float(gpa)
+print(f"my name is : {name}")
+print(f"Age : {age}")
+print(f"gpa : {gpa}")
+print(f"Are you student : {is_student}")
