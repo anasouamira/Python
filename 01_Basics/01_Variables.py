ab11d115eb253daaa760d7133a50b52af02de4dd
Ouamira <anasouamira@gmail.com>
2024-11-21 16:42:13 +0100

    Created By Anas Ouamira

---
+++ 01_Basics/01_Variables.py
@@ -0,0 +1,25 @@
+# Variable = A container for a value (string, integer, float, Boolean)
+#            A variable behaves as if it was the value it contains.
+
+
+# Strings :
+FirstName = "Anas"
+print(f"Hello {FirstName}")
+
+# Integers :
+Age = 20 
+print(f"I am {Age} years old")
+
+# Float : 
+price = 10.99
+print(f"the price is {price}$")
+
+# Boolean : 
+is_student = True # Or false
+print(f"Are you a student? : {is_student} ")
+
+if is_student : 
+    print("you are a student")
+else: 
+    print("You are NOT student")
+
