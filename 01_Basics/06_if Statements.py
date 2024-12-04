0c2ced509583ef60ed5fa2d993b97da5bacba5b4
Ouamira <anasouamira@gmail.com>
2024-12-04 18:32:15 +0100

    Created By Anas Ouamira

---
+++ 01_Basics/06_if Statements.py
@@ -0,0 +1,9 @@
+number = 10  # Declare a variable
+
+# Check the value of number with multiple conditions
+if number > 10:
+    print("Number is greater than 10")
+elif number == 10:
+    print("Number is equal to 10")  # This will execute since number is 10
+else:
+    print("Number is less than 10")
